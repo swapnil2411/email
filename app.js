13aaaee3e35c7f5e864860268da5352ec0eabc62
Swapnil2411 <swapnildarge65@gmail.com>
2024-01-31 20:13:03 +0530

    Port Changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 const port = process.env.PORT || 443;
 
 app.get("/", (req,res) => {
-    res.send("Server Start")
+    res.send("Server Start on: ", port)
 })
 
 app.use(express.json());
